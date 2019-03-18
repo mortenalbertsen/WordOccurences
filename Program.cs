1a3f0df8c0bdcb3191dcb45cdcf253f0b3618fc7
Morten Albertsen <morten.albertsen@me.com>
2019-03-18 17:12:26 +0100

    Implemented solution in C# (rather than F#), Updated README, supplied sample-text files

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WordOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read in file
+            var fileName = args[1];
+            Console.WriteLine("Filename: " + fileName);
+            var reader = new StreamReader(fileName);
+
+            String nextLine = reader.ReadLine();
+            var wordOccurences = new Dictionary<String, int>();
+            while (nextLine != null)
+            {
+                // Split line
+                var wordsInLine = nextLine.Trim().Split(' ');
+                foreach (var word in wordsInLine)
+                {
+                    if (string.IsNullOrEmpty(word))
+                    {
+                        continue;
+                    }
+                    int possibleValue;
+                    Console.WriteLine("Viewing: \"" + word + "\"");
+                    var lowerCasedWord = word.ToLower();
+                    if (wordOccurences.TryGetValue(lowerCasedWord, out possibleValue))
+                    {
+                        wordOccurences[lowerCasedWord] = possibleValue + 1;
+                    }
+                    else
+                    {
+                        wordOccurences[lowerCasedWord] = 1;
+                    }
+                }
+                nextLine = reader.ReadLine();
+            }
+
+            // Close stream
+            reader.Close();
+
+            // Print results
+            foreach (KeyValuePair<string, int> entry in wordOccurences)
+            {
+                Console.WriteLine(entry.Value + ": " + entry.Key);
+            }
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # WordOccurences
 
-Functional program written in F# that given an input text file prints word-occurences. 
+Functional program written in C# that given an input text file prints word-occurences. 
+
+
+
+## How to run
+
+From root-folder:
+
+1. Build
+
+   ```bash
+   dotnet build
+   ```
+
+2. Run
+
+   ```bash
+   dotnet run bin/Debug/netcoreapp2.2/WordOccurences.dll <fileToTest>
+   ```
 
